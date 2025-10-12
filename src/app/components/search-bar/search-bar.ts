df92a8984e3042ce0053c18c490ea15cdabbbf5a
Th-Guasti <thaslesouza@gmail.com>
2025-10-12 13:52:22 -0300

    search bar visualmente pronto

--- src/app/components/search-bar/search-bar.ts
+++ src/app/components/search-bar/search-bar.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-search-bar',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, MatInputModule, MatButtonModule, MatIconModule, FormsModule],
   templateUrl: './search-bar.html',
-  styleUrl: './search-bar.scss'
+  styleUrls: ['./search-bar.scss'],
 })
 export class SearchBar {
+  searchText = signal('');
 
+  @Output() search = new EventEmitter<string>();
+
+  onSearch() {
+    const value = this.searchText();
+    if (value && value.trim().length > 0) {
+      this.search.emit(value.trim());
+      // opcional: limpar o input
+      // this.searchText.set('');
+    }
+  }
 }
