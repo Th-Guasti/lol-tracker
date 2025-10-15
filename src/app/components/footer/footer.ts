92681523b1e53e5fde969280852622c4c50a3c85
Th-Guasti <thaslesouza@gmail.com>
2025-10-15 01:07:21 -0300

    footer com links

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [MatToolbarModule, MatIconModule],
   templateUrl: './footer.html',
-  styleUrl: './footer.scss'
+  styleUrl: './footer.scss',
 })
-export class Footer {
-
-}
+export class Footer {}
